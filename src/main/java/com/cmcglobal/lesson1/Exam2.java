156d7e16a57f6fb7d5af92f1408d75a292c94133
Đặng Xuân Long <longdang@meu-solutions.com>
2025-02-19 21:35:17 +0700

    firrt commit

---
+++ src/main/java/com/cmcglobal/lesson1/Exam2.java
@@ -0,0 +1,23 @@
+package com.cmcglobal.lesson1;
+
+public class Exam2 {
+    public static void main(String[] args) {
+        int a = -5;
+        int b = 8;
+        int c = 6;
+        int d = 55;
+        int e = 9;
+        int f = 20;
+        int g = 3;
+        int i = 5;
+        int k =-3;
+        int m =15;
+        int n = 2;
+        int l = 3;
+
+        System.out.println(a + b *c);
+        System.out.println((d+e)% e);
+        System.out.println(f +k*i /b);
+        System.out.println(i +m /g *n - b% l);
+    }
+}
---
+++ src/main/java/com/cmcglobal/lesson2/Main.java
@@ -0,0 +1,14 @@
+package com.cmcglobal.lesson2;
+
+public class Main {
+    public static void main(String[] args) {float x = 5;
+        String Ho = " Dang";
+        String Dem = "Xuan";
+        String Ten= "Long";
+
+        System.out.println(Ho + Dem + Ten);
+
+
+
+}}
+
